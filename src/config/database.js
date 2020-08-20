83c21ae0584333ca6d6b98f5743b80e34c845d10
mpinheiro <mpinheiro@orion.tech>
2020-08-19 21:40:41 -0300

    config docker, database

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'authtdd',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  loggin: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
