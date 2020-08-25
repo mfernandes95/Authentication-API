d2b5ae0aa789665836ae87599fe2839689e6fc96
mpinheiro <mpinheiro@orion.tech>
2020-08-25 13:52:00 -0300

    feat factory and faker

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -5,20 +5,34 @@ const app = require("../../src/app");
 const { User } = require("../../src/app/models");
 
 const truncate = require("../utils/truncate");
+const factory = require("../factories");
 
 describe("Authentication", () => {
   beforeEach(async () => {
     await truncate();
   });
 
-  // it("It should received JWT token when authenticated with valid credentials", () => {});
+  // it("should authenticate with valid credentials", async () => {
+  //   const user = await factory.create("User", {
+  //     password: "123123",
+  //   });
 
+  //   const response = await request(app).post("/sessions").send({
+  //     email: user.email,
+  //     password: "123123",
+  //   });
+
+  //   expect(response.status).toBe(200);
+  // });
   it("should authenticate with valid credentials", async () => {
-    const user = await User.create({
-      name: "Matheus",
-      email: "matheuss@email.com",
+    const user = await factory.create("User", {
       password: "123456",
     });
+    // const user = await User.create({
+    //   name: "Matheus",
+    //   email: "matheuss@email.com",
+    //   password: "123456",
+    // });
 
     const response = await request(app).post("/sessions").send({
       email: user.email,
@@ -29,24 +43,20 @@ describe("Authentication", () => {
   });
 
   it("should not authenticate with invalid credentials", async () => {
-    const user = await User.create({
-      name: "Matheus",
-      email: "matheuss@email.com",
-      password: "123123",
+    const user = await factory.create("User", {
+      password: "123456",
     });
 
     const response = await request(app).post("/sessions").send({
       email: user.email,
-      password: "123456",
+      password: "123123",
     });
 
     expect(response.status).toBe(401);
   });
 
   it("should return jwt token when authenticated", async () => {
-    const user = await User.create({
-      name: "Matheus",
-      email: "matheuss@email.com",
+    const user = await factory.create("User", {
       password: "123123",
     });
 
